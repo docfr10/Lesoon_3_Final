3b1bdf5eeb8cb9a98438add32c6b4a48b5fcc07e
docfr10 <docfr10@yandex.ru>
2025-10-24 09:50:23 +0300

    Add functions

--- src/Main.kt
+++ src/Main.kt
@@ -1,9 +1,34 @@
 fun main() {
     // Try-catch
-    divide(6,2)
-    divide(3,0)
+    divide(6, 2)
 
+    // Функции области видимости
+    // Позволяет исполнить блок кода и записать результат
+    // его выполнения в отдельную переменную
 
+    val name = "Hello"
+    // let
+    val nameLength = name.let {
+        println(it.length)
+        it.length
+    }
+    println(nameLength)
+
+    // run
+    val nemName = "Name"
+    val nemNameLength = nemName.run {
+        println(this.length)
+        this.length
+    }
+
+    // with
+    val string = StringBuilder()
+    // Нужно для многократного обращения к переменной,
+    // чтобы каждый раз не писать ее название
+    with(string) {
+        append("Hello")
+        append("World")
+    }
 }
 
 fun divide(a: Int, b: Int): Int {
