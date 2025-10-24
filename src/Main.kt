187758455e93b4a6ebad453fbc6503810a1a2130
docfr10 <docfr10@yandex.ru>
2025-10-24 09:36:53 +0300

    Try-catch commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,22 @@
 fun main() {
-    // New comment
+    // Try-catch
+    divide(6,2)
+    divide(3,0)
+
+
+}
+
+fun divide(a: Int, b: Int): Int {
+    return try {
+        // В блоке try происходит исполняемый код
+        println(a / b)
+        a / b
+    } catch (e: Error) {
+        // Блок обработчик ошибок
+        println("Возникла следующая ошибка: ${e.message}")
+        0
+    } finally {
+        // Блок finally выполнится в любом случае
+        println("Блок finally сработал")
+    }
 }
